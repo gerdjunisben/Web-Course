5b43f13c2cff0da7c80976f664825e6217774016
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2022-11-28 15:50:00 -0500

    be scared, I'm going to finish up to react by Monday

--- Nodejs/EJS-module/app.js
+++ Nodejs/EJS-module/app.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/day.js")
 
 
 const app = express();
-
+let items=[];
 app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req,res){
-    var today = new Date();
-
-    var items=[];
     
-    var options ={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-
-    var day=today.toLocaleDateString("en-US");
-
+    let day = date();
     res.render("list",{kindOfDay:day,newListItem:items});
 
     res.send();
 });
 
 app.post("/", function(req,res){
-    var item =req.body.newItem;
+    let item =req.body.newItem;
     items.push(item);
     res.redirect("/");
 })
+
+
 
 app.listen(3000,function(){
     console.log("Heard");
